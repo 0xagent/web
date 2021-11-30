c6bc4ea75fafeaf262d942260daac83a0c9f60c5
0xagent <95272752+0xagent@users.noreply.github.com>
2021-11-29 23:49:21 -0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -33,3 +33,19 @@ $('.image-flip').click(function(){
   $(this).toggleClass('hover')
 
 })
+function findGetParameter(parameterName) {
+  var result = null,
+      tmp = [];
+  var items = location.search.substr(1).split("&");
+  for (var index = 0; index < items.length; index++) {
+      tmp = items[index].split("=");
+      if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+  }
+  return result;
+}
+window.onload = function(){
+  if (findGetParameter("services")!=null){
+    $('.form-received').toggleClass('show')
+  }
+
+}
